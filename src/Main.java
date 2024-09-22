eecbe9677d386a84c8c540c37cc2fb643df1a445
timadcock <timthetiger98@yahoo.com>
2024-09-22 15:35:43 -0500

    Rudimentary multithreading

--- src/Main.java
+++ src/Main.java
@@ -12,25 +12,38 @@ public class Main {
         byte[]     data = {1, 2, 3};
         BigInteger bi   = new BigInteger("0");
 
-//        block_chain.Block b = new block_chain.Block(bi, data,
-//                                                    new Date().getTime()
-//        );
-//        block_chain.Block b1 = new block_chain.Block(b.getHash(), data,
-//                                                     new Date().getTime()
-//        );
-
-
         Store st = new Store("./db.txt");
 
         Chain c = new Chain(
                 new Link(new Block(bi, data, new Date().getTime()), null));
 
-        c.insert(new Block(c.newest_hash(), data, new Date().getTime()), 5);
-        c.insert(new Block(c.newest_hash(), data, new Date().getTime()), 5);
-        c.insert(new Block(c.newest_hash(), data, new Date().getTime()), 5);
+        long startTime = System.nanoTime();
+        long total     = 0L;
+        c.insert(new Block(c.newest_hash(), data, new Date().getTime()), 25);
+        long endTime = System.nanoTime();
+        System.out.println((endTime - startTime) / 1000000 + "ms");
+
+        total = endTime - startTime;
+
+
+        startTime = System.nanoTime();
+        c.insert(new Block(c.newest_hash(), data, new Date().getTime()), 25);
+        endTime = System.nanoTime();
+        System.out.println((endTime - startTime) / 1000000 + "ms");
+
+        total += endTime - startTime;
+
+
+        startTime = System.nanoTime();
+        c.insert(new Block(c.newest_hash(), data, new Date().getTime()), 25);
+        endTime = System.nanoTime();
+        System.out.println((endTime - startTime) / 1000000 + "ms");
+
+        total += endTime - startTime;
+        System.out.println(((total / 3) / 1000000) + "ms");
+
 
         st.store_chain(c);
-
 
         System.out.println(st.retrieve_chain().validate_chain());
         System.out.println(st.retrieve_chain());
--- src/block_chain/Block.java
+++ src/block_chain/Block.java
@@ -7,28 +7,29 @@ import java.security.NoSuchAlgorithmException;
 
 // Object to hold block data
 public class Block implements Serializable {
-    private final BigInteger prevHash;
-    private final byte[]     data;
-    private final long       timestamp;
-    private       BigInteger hash;
-    private       int        nonce;
+    static volatile int        nonce_found = -1;
+    private final   BigInteger prevHash;
+    private final   byte[]     data;
+    private final   long       timestamp;
+    private         BigInteger hash;
+    private         long       nonce;
 
     public Block(BigInteger prevHash, byte[] data, long timestamp) {
         this.prevHash  = prevHash;
         this.data      = data;
         this.timestamp = timestamp;
         this.nonce     = 0;
-        this.hash      = generateHash();
+        this.hash      = generateHash(this.nonce);
     }
 
-    private BigInteger generateHash() {
+    private BigInteger generateHash(long nonce) {
 
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-512");
 
             String string_to_be_hash =
                     this.prevHash.toString(16) + this.timestamp +
-                    Long.toString(this.data.length) + this.nonce;
+                    Long.toString(this.data.length) + nonce;
 
             byte[] msgDigest = digest.digest(string_to_be_hash.getBytes());
 
@@ -46,20 +47,43 @@ public class Block implements Serializable {
     // Proof will be determined by a hash that is xonred by a value with x
     // number of zeroes at the least significant end.
     public void provideProof(int difficulty) {
-
+        nonce_found = -1;
         BigInteger proof = BigInteger.ZERO.not();
         proof = proof.shiftLeft(difficulty).not();
-        boolean found = false;
 
-        while (!found) {
-            BigInteger anded = this.hash.not().and(proof);
-            if (anded.equals(proof)) {
-                found = true;
-            } else {
-                this.nonce++;
-                this.hash = generateHash();
-            }
+        int          num_threads = 8;
+        HashFinder[] threads     = new HashFinder[num_threads];
+
+        long seperation = 4294967296L / num_threads;
+
+        for (int i = 0; i < num_threads; i++) {
+            threads[i] = new HashFinder(Long.MIN_VALUE + (seperation * i),
+                                        this.hash, proof, i
+            );
+            threads[i].start();
         }
+
+
+        while (nonce_found == -1) {
+            //System.out.println("Waiting on threads...");
+        }
+
+        for (int i = 0; i < num_threads; i++) {
+            threads[i].interrupt();
+        }
+
+        this.nonce = threads[nonce_found].nonce;
+        this.hash  = generateHash(this.nonce);
+        System.out.println(difficulty);
+
+//        while (!test_hash.not().and(proof).equals(proof)) {
+//
+//            this.nonce++;
+//            test_hash = generateHash();
+//
+//        }
+
+        // this.hash = test_hash;
 
     }
 
@@ -83,4 +107,39 @@ public class Block implements Serializable {
     public String toString() {
         return String.format("Hash: %s\n", this.hash.toString(16));
     }
+
+    private class HashFinder extends Thread {
+        private final int        ID;
+        public        long       nonce;
+        public        BigInteger hash;
+        public        BigInteger proof;
+
+
+        public HashFinder(long nonce, BigInteger hash, BigInteger proof,
+                          int id) {
+            this.hash  = hash;
+            this.proof = proof;
+            this.ID    = id;
+            this.nonce = nonce;
+
+
+        }
+
+        public void run() {
+            while (!this.hash.not().and(this.proof).equals(this.proof) &&
+                   nonce_found == -1) {
+                this.nonce++;
+
+                // Not really a true nonce but I want to be able to have
+                // valid threads use
+                this.hash = generateHash(this.nonce);
+            }
+
+            if (nonce_found == -1) {
+                nonce_found = this.ID;
+            }
+        }
+    }
+
+
 }
